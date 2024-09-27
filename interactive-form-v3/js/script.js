0e3f0ef4d9236877f81531bf8318ee07fb542ca8
jilliandike <43615050+jilliandike@users.noreply.github.com>
2024-09-27 15:24:35 -0600

    Added dynamic color select menu

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -13,3 +13,25 @@ jobRole.addEventListener('change', (e) => {
     }
 });
 
+const shirtDesign = document.getElementById("design");
+const shirtColorSelect = document.getElementById("color");
+const colorOptions = shirtColorSelect.children;
+
+shirtColorSelect.disabled = true; 
+
+shirtDesign.addEventListener('change', (e) => {
+    shirtColorSelect.disabled = false; 
+
+    for (let i  = 0; i < colorOptions.length; i++) {
+        const value = e.target.value;
+        const optionTheme = colorOptions[i].getAttribute("data-theme");
+
+        if (optionTheme === value) {
+            colorOptions[i].hidden = false;
+            colorOptions[i].setAttribute = true;
+        } else {
+            colorOptions[i].hidden = true;
+            colorOptions[i].setAttribute = false; 
+        }
+    }
+});
