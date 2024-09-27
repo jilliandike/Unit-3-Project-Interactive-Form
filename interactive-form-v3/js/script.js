893bce6cda9bf8403ad719f9ffeaafbe57ce710a
jilliandike <43615050+jilliandike@users.noreply.github.com>
2024-09-27 13:50:09 -0600

    Added focus to name field and changed "other job role" display

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,1 +1,15 @@
-console.log(`Test`)
+const nameField = document.getElementById("name");
+nameField.focus();
+
+const jobRole = document.getElementById("title");
+const otherJobRole = document.getElementById("other-job-role");
+
+otherJobRole.style.display = "none"; 
+jobRole.addEventListener('change', (e) => {
+    if (e.target.value === "other") {
+        otherJobRole.style.display = ""; 
+    } else {
+        otherJobRole.style.display = "none"; 
+    }
+});
+
